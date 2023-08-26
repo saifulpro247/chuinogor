37a0feab15b96ce6e1f1ae99e0a1da3c49f937a9
Md. Saiful <saifulpro247@gmail.com>
2023-08-26 18:53:48 +0600

    this is chuinogor

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,17 @@
     // modal video
     $(".order-video").modalVideo({channel:'youtube'});
 
-    
+
+         // tab js
+         $('.tab-header').click(function() {
+          const tabId = $(this).data('tab');
+          $('.tab-header').removeClass('active');
+          $(this).addClass('active');
+          $('.tab-content').removeClass('active-tab').hide();
+          $('#' + tabId).addClass('active-tab').show();
+        });
+
+        $('.tab-header[data-tab="tab1"]').click();
+      
 
 })($);
